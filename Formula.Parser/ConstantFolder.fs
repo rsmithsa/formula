bf55472b13dc8745075b4475e6720d114a82232c
rsmithsa <richard@smith-sa.com>
2018-02-05 20:34:08 +0200

    Support for logical expressions #7 - Updated constant folder

--- Formula.Parser/ConstantFolder.fs
+++ Formula.Parser/ConstantFolder.fs
@@ -74,10 +74,41 @@ module ConstantFolder =
                 Constant(Boolean(not value))
             | _ -> Inversion(res)
         | Comparison (a, op, b) ->
-            Comparison(a, op, b)
+            let resA = foldConstants a
+            let resB = foldConstants b
+            match (resA, resB) with
+            | (Constant cA, Constant cB) ->
+                let valueA = castToDouble cA
+                let valueB = castToDouble cB
+
+                match op with
+                | Equal ->
+                    Constant(Boolean(valueA = valueB))
+                | NotEqual ->
+                    Constant(Boolean(valueA <> valueB))
+                | GreaterThan ->
+                    Constant(Boolean(valueA > valueB))
+                | LessThan ->
+                    Constant(Boolean(valueA < valueB))
+                | GreaterThanEqual ->
+                    Constant(Boolean(valueA >= valueB))
+                | LessThanEqual ->
+                    Constant(Boolean(valueA <= valueB))
+            | _ -> Comparison(resA, op, resB)
         | Logical (a, op, b) ->
-            Logical(a, op, b)
+            let resA = foldConstants a
+            let resB = foldConstants b
+            match (resA, resB) with
+            | (Constant cA, Constant cB) ->
+                let valueA = castToBool cA
+                let valueB = castToBool cB
+
+                match op with
+                | And ->
+                    Constant(Boolean(valueA && valueB))
+                | Or ->
+                    Constant(Boolean(valueA || valueB))
+            | _ -> Logical(resA, op, resB)
         | Function (f, args) ->
             let res = args |> List.map foldConstants
             Function(f, res)
-
