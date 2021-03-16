cb27310a825c5f9fcfa492842e5d12538d0033b9
rsmithsa <richard@smith-sa.com>
2021-03-16 14:23:46 +0200

    Extend the supported types for function parameters to include strings #28 - Equality operators for compiler/C#

--- Formula.Parser/Ast.fs
+++ Formula.Parser/Ast.fs
@@ -18,6 +18,13 @@ module Ast =
                static member inline op_Implicit(x: bool) = Boolean(x)
                static member inline op_Implicit(x: string) = Text(x)
 
+               static member op_Equality(a: value, b: value) = a = b
+               static member op_Inequality(a: value, b: value) = a <> b
+               static member op_LessThanOrEqual(a: value, b: value) = a <= b
+               static member op_GreaterThanOrEqual(a: value, b: value) = a >= b
+               static member op_LessThan(a: value, b: value) = a < b
+               static member op_GreaterThan(a: value, b: value) = a > b
+
     type arithmetic = Add | Subtract | Multiply | Divide | Modulus | Power
     type comparison = Equal | NotEqual | GreaterThan | LessThan | GreaterThanEqual | LessThanEqual
     type logical = And | Or
