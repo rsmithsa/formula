d487da6a09882215bb996d42790d704be8cbf486
rsmithsa <richard@smith-sa.com>
2021-03-17 17:16:23 +0200

    Return range information from the dependency extractor #34

--- Formula.Parser.Tests/DependencyExtractorTests.fs
+++ Formula.Parser.Tests/DependencyExtractorTests.fs
@@ -126,3 +126,124 @@ type DependencyExtractorTests () =
             Assert.AreEqual(expected, deps);
         | Failure (msg, error, userState) ->
             Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestConstantDependenciesWithRanges () =
+        let result = parseFormulaString "42"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected = List.empty<identifier * option<expr * expr>>
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestConstantRangeDependenciesWithRanges () =
+        let result = parseFormulaString "[My Var]|1:0|"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("My Var"), Some(Constant(Number(1.0)), Constant(Number(0.0)))) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestVariableDependenciesWithRanges () =
+        let result = parseFormulaString "[My Var]"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("My Var"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestVariableRangeDependenciesWithRanges () =
+        let result = parseFormulaString "[My Var]|A:B|"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("B"), None); (Identifier("A"), None); (Identifier("My Var"), Some(Variable(Identifier("A"), None), Variable(Identifier("B"), None))) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestFunctionDependenciesWithRanges () =
+        let result = parseFormulaString "POW(SQRT([My Var]), [A])"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("My Var"), None); (Identifier("A"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestBranchDependenciesWithRanges () =
+        let result = parseFormulaString "IF [My Var] THEN [A] ELSE B"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("B"), None); (Identifier("A"), None); (Identifier("My Var"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestComparisonDependenciesWithRanges () =
+        let result = parseFormulaString "-[My Var] = [A]"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("A"), None); (Identifier("My Var"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestLogicalDependenciesWithRanges () =
+        let result = parseFormulaString "[My Var] || ![A]"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("A"), None); (Identifier("My Var"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestNegationDependenciesWithRanges () =
+        let result = parseFormulaString "-[A]"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("A"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestInversionDependenciesWithRanges () =
+        let result = parseFormulaString "![A]"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("A"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestArithmeticDependenciesWithRanges () =
+        let result = parseFormulaString "[My Var] * [A] % B"
+        match result with
+        | Success (ast, userState, endPos) ->
+            let deps = extractDependenciesWithRanges ast []
+            let expected: list<identifier * option<expr * expr>> = [ (Identifier("B"), None); (Identifier("A"), None); (Identifier("My Var"), None) ]
+            Assert.AreEqual(expected, deps);
+        | Failure (msg, error, userState) ->
+            Assert.Fail(msg)
--- Formula.Parser/DependencyExtractor.fs
+++ Formula.Parser/DependencyExtractor.fs
@@ -35,3 +35,29 @@ module DependencyExtractor =
         | Branch (cond, a, b) ->
             extractDependencies b (extractDependencies a (extractDependencies cond deps))
 
+    let rec extractDependenciesWithRanges ast deps =
+
+        match ast with
+        | Constant c ->
+            deps
+        | Variable (v, r) ->
+            let d = (v, r)
+            match r with
+            | Some (a, b) ->
+                (extractDependenciesWithRanges b (extractDependenciesWithRanges a (d::deps)))
+            | None -> d::deps
+        | Negation n ->
+            extractDependenciesWithRanges n deps
+        | Arithmetic (a, op, b) ->
+            extractDependenciesWithRanges b (extractDependenciesWithRanges a deps)
+        | Inversion i ->
+            extractDependenciesWithRanges i deps
+        | Comparison (a, op, b) ->
+            extractDependenciesWithRanges b (extractDependenciesWithRanges a deps)
+        | Logical (a, op, b) ->
+            extractDependenciesWithRanges b (extractDependenciesWithRanges a deps)
+        | Function (f, args) ->
+            args |> List.map (fun a -> extractDependenciesWithRanges a deps) |> List.concat
+        | Branch (cond, a, b) ->
+            extractDependenciesWithRanges b (extractDependenciesWithRanges a (extractDependenciesWithRanges cond deps))
+
